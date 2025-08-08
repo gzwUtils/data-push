37cab77cadf647bb8cb24c37fe99ed6d88d842fe
gaozhiwei <gzw15365@cnki.net>
2025-08-08 11:33:11 +0800

    修改

---
+++ data-push-web/src/main/java/kd/data/web/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package kd.data.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域问题配置
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET","POST","PUT","OPTIONS")
+                .allowCredentials(true)
+                .exposedHeaders("*")
+                .maxAge(3600)
+        ;
+    }
+
+}
---
+++ data-push-web/src/main/java/kd/data/web/entities/JdbcTargetMessage.java
@@ -0,0 +1,53 @@
+package kd.data.web.entities;
+
+import kd.data.core.customer.annotation.ConsumerField;
+import kd.data.core.customer.annotation.ConsumerTarget;
+import kd.data.core.customer.annotation.CustomerTableName;
+import kd.data.core.customer.target.targetenums.TargetEnums;
+import lombok.Data;
+
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
+/**
+ * @author gaozw
+ * @date 2025/8/7 17:17
+ */
+@ConsumerTarget(TargetEnums.JDBC)
+@CustomerTableName("tar_message")
+@Data
+public class JdbcTargetMessage {
+
+    @ConsumerField(value = "id", role = ConsumerField.FieldRole.ID)
+    private BigInteger id;
+
+    @ConsumerField("room_id")
+    private BigInteger roomId;
+
+    @ConsumerField("from_uid")
+    private BigInteger fromUid;
+
+    @ConsumerField("content")
+    private String content;
+
+    @ConsumerField("reply_msg_id")
+    private BigInteger replyMsgId;
+
+    @ConsumerField("status")
+    private Integer status;
+
+    @ConsumerField("gap_count")
+    private Integer gapCount;
+
+    @ConsumerField("type")
+    private Integer type;
+
+    @ConsumerField("extra")
+    private String extra;
+
+    @ConsumerField("create_time")
+    private LocalDateTime createTime;
+
+    @ConsumerField( "update_time")
+    private LocalDateTime updateTime;
+}

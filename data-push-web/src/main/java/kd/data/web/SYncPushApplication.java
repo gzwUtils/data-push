c7860fa8a7254b235cb3115a847792c0830dbdc4
gaozhiwei <gzw15365@cnki.net>
2025-08-15 15:08:38 +0800

    移除测试

--- data-push-web/src/main/java/kd/data/web/SYncPushApplication.java
+++ data-push-web/src/main/java/kd/data/web/SYncPushApplication.java
@@ -3,10 +3,11 @@ package kd.data.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = {"kd.data.core", "kd.data.service", "kd.data.web"})
 public class SYncPushApplication {
     public static void main(String[] args) {
